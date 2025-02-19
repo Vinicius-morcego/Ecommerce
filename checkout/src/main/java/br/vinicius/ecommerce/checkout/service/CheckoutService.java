720272144f3625f7d87fe0423bb3f4fcc8f0d53f
Vinicius Souza <vinicius.souza@novaxs.com.br>
2025-02-19 16:20:10 -0300

    Adicionando as camadas de Entity, Repository e adicionando a dependencia Spring Data Jpa

--- checkout/src/main/java/br/vinicius/ecommerce/checkout/service/CheckoutService.java
+++ checkout/src/main/java/br/vinicius/ecommerce/checkout/service/CheckoutService.java
@@ -1,4 +1,10 @@
 package br.vinicius.ecommerce.checkout.service;
 
+import br.vinicius.ecommerce.checkout.entity.CheckoutEntity;
+import br.vinicius.ecommerce.checkout.resource.checkout.CheckoutRequest;
+
+import java.util.Optional;
+
 public interface CheckoutService {
+    Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest);
 }
--- checkout/src/main/java/br/vinicius/ecommerce/checkout/service/CheckoutServiceImpl.java
+++ checkout/src/main/java/br/vinicius/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -1,4 +1,19 @@
 package br.vinicius.ecommerce.checkout.service;
 
+import br.vinicius.ecommerce.checkout.entity.CheckoutEntity;
+import br.vinicius.ecommerce.checkout.repository.CheckoutRepository;
+import br.vinicius.ecommerce.checkout.resource.checkout.CheckoutRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CheckoutServiceImpl implements CheckoutService{
+
+    private final CheckoutRepository checkoutRepository;
+
+    @Override
+    public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
+        return Optional.empty();
+    }
 }
